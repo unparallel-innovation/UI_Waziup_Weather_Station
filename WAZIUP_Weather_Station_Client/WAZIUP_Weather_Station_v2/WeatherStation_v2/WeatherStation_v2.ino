85907945916c1dfe8241f51237b22b734f9f778c
Sérgio Rocha <sergio.rocha@unparallel.pt>
2017-04-07 15:38:58 +0100

    Update
    ADC table values update

--- WAZIUP_Weather_Station_Client/WAZIUP_Weather_Station_v2/WeatherStation_v2/WeatherStation_v2.ino
+++ WAZIUP_Weather_Station_Client/WAZIUP_Weather_Station_v2/WeatherStation_v2/WeatherStation_v2.ino
@@ -190,6 +190,7 @@ void setup()
   rtc.begin();    // Begin the RTC. Get Date & Time from system
   
   // Uncomment to adjust the date & time ONLY in the first compiler run
+  //rtc.adjust(DateTime( F(__DATE__), F(__TIME__) ) - TimeSpan(0,1,0,0));
   if (rtc.lostPower())
   {
     rtc.adjust(DateTime( F(__DATE__), F(__TIME__) ) - TimeSpan(0,1,0,0));   // Get the Date and Time the compiler was run
@@ -351,22 +352,22 @@ int get_wind_direction()
   // Each threshold is the midpoint between adjacent headings. The output is degrees for that ADC reading.
   // Note that these are not in compass degree order! See Weather Meters datasheet for more information.
 
-  if (adc < 380) return (113);
-  if (adc < 393) return (68);
-  if (adc < 414) return (90);
-  if (adc < 456) return (158);
-  if (adc < 508) return (135);
-  if (adc < 551) return (203);
-  if (adc < 615) return (180);
-  if (adc < 680) return (23);
-  if (adc < 746) return (45);
-  if (adc < 801) return (248);
-  if (adc < 833) return (225);
-  if (adc < 878) return (338);
-  if (adc < 913) return (0);
-  if (adc < 940) return (293);
-  if (adc < 967) return (315);
-  if (adc < 990) return (270);
+  if (adc < 365) return (113); // 380
+  if (adc < 382) return (68); //  393
+  if (adc < 390) return (90); //  414
+  if (adc < 423) return (158); // 456
+  if (adc < 476) return (135); //508 
+  if (adc < 529) return (203); //  551
+  if (adc < 563) return (180); // 615
+  if (adc < 657) return (23); //  680
+  if (adc < 697) return (45); //  746
+  if (adc < 790) return (248); //  801
+  if (adc < 809) return (225); //  833
+  if (adc < 856) return (338); // 878 
+  if (adc < 905) return (0); //  913
+  if (adc < 927) return (293); // 940
+  if (adc < 960) return (315); //  967
+  if (adc < 991) return (270); // 990
   return (-1); // error, disconnected?
 }
 
